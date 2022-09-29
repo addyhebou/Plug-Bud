bd9ac6f29486c65cf02d810f99f698d01cf4b3f3
Addy Hebou <addyhebou@gmail.com>
2022-09-29 00:58:42 -0700

    [Clickable-Cards]: (FEAT, STYLE): Made cards clickable

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import '../styles/ArtistCard.scss';
 
-export default function ArtistCard({ name, imgData, genres }) {
+export default function ArtistCard({ name, imgData, genres, spotifyLink }) {
   return (
-    <div className="artistCard">
+    <a
+      className="artistCard"
+      href={spotifyLink}
+      target="_blank"
+      rel="noreferrer"
+    >
       <div className="text">
         <h3>{name}</h3>
         {genres?.length > 0 && (
           <p className="genres">Genres: {genres.join(', ')}</p>
         )}
       </div>
-      <img src={imgData.url} width={imgData.width} height={imgData.height} />
-    </div>
+      <img
+        src={imgData.url}
+        width={imgData.width}
+        height={imgData.height}
+        alt={`The artist named ${name}}`}
+      />
+    </a>
   );
 }
